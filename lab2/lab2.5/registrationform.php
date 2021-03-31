bdfccb91db71a93458cfa7d4ea0a0e9cde5335fc
phamtuanson179 <62193655+phamtuanson179@users.noreply.github.com>
2021-03-31 21:46:42 +0700

    lab2

---
+++ lab2/lab2.5/registrationform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>A Simple Form</title>
+  </head>
+  <body>
+    <form action="http://fit.hut.edu.vn/~trangntt/courses/wp" method="post">
+      Click submit to start our initial PHP program.
+      <br>
+      <input type="submit" value="Click To Submit">
+      <input type="reset" value="Erase and Restart">
+    </form>
+  </body>
+</html>
